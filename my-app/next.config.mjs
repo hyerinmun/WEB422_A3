b8c4bb3f7ef08c09f436c8a926a4d35a5acb68e0
HyerinMun <hmun@myseneca.ca>
2025-02-12 10:16:10 -0500

    d

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,13 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/(.*)",
-        destination: "/",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
